4cca0a17928474235cbd728b68b8642b2b74aa3b
Gustav Nilsonne <gustav.nilsonne@ki.se>
2016-04-20 13:35:44 +0200

    Updated with log amplitude

--- Resting State/Global_signal/Global signal.R
+++ Resting State/Global_signal/Global signal.R
@@ -38,15 +38,27 @@ for (i in 1:length(files_S4)){
 
 ########################
 
-# Calculate mean global signal and put in data frame
+# Calculate mean global signal and other measures and put in data frame
 fun_rms <- function(x) sqrt(mean(x^2))
 
 data_glob <- data.frame(
   ID = rep(1:53, 4),
   session = c(rep(1, 53), rep(2, 53), rep(3, 53), rep(4, 53)),
   globalsignal = c(unlist(lapply(covs_S1, mean)), unlist(lapply(covs_S2, mean)), unlist(lapply(covs_S3, mean)), unlist(lapply(covs_S4, mean))),
-  globalsignal_rms = c(unlist(lapply(covs_S1, fun_rms)), unlist(lapply(covs_S2, fun_rms)), unlist(lapply(covs_S3, fun_rms)), unlist(lapply(covs_S4, fun_rms)))
+  globalsignal_rms = c(unlist(lapply(covs_S1, fun_rms)), unlist(lapply(covs_S2, fun_rms)), unlist(lapply(covs_S3, fun_rms)), unlist(lapply(covs_S4, fun_rms))),
+  globalsignal_sd = c(unlist(lapply(covs_S1, sd)), unlist(lapply(covs_S2, sd)), unlist(lapply(covs_S3, sd)), unlist(lapply(covs_S4, sd)))
 )
+
+hist(data_glob$globalsignal)
+hist(data_glob$globalsignal_rms)
+hist(data_glob$globalsignal_sd)
+hist(log(data_glob$globalsignal_sd))
+
+data_glob$globalsignal_logsd <- log(data_glob$globalsignal_sd) # Log transform to achieve a more normal distribution
+
+gs <- c(unlist(covs_S1), unlist(covs_S2), unlist(covs_S3), unlist(covs_S4))
+
+hist(gs)
 
 ########################
 
@@ -116,6 +128,7 @@ data_glob$condition <- relevel(data_glob$condition, ref = "fullsleep")
 require(nlme)
 require(effects)
 require(RColorBrewer)
+cols <- brewer.pal(n = 3, name = "Dark2")
 
 # Full model
 lm1 <- lme(globalsignal_rms ~ condition * AgeGroup + FD, data = data_glob, random = ~1|ID)
@@ -123,19 +136,18 @@ summary(lm1)
 intervals(lm1)
 plot(effect("condition*AgeGroup", lm1))
 
-cols <- brewer.pal(n = 3, name = "Dark2")
-
-pdf("GS.pdf") 
-par(mar = c(4, 4, 0, 0))
-plot(1, frame.plot = F, xlim = c(0, 1), ylim = c(4100, 4400), xlab = "", ylab = "Global signal intensity", xaxt = "n", type = "n")
+# Make figure
+pdf("GS1.pdf", height = 6, width = 6) 
+par(mar = c(4, 5, 1, 2))
+plot(1, frame.plot = F, xlim = c(0, 1), ylim = c(4100, 4400), xlab = "", ylab = "Global signal intensity (rms)", xaxt = "n", type = "n")
 axis(1, at = c(0.05, 0.95), labels = c("Full sleep", "Sleep deprived"))
-lines(x = c(0, 0.9), y = effect("condition*AgeGroup", lm1)$fit[1:2], pch = 16, col = cols[3], type = "b")
-lines(x = c(0.1, 1), y = effect("condition*AgeGroup", lm1)$fit[3:4], pch = 16, col = cols[2], type = "b")
-lines(x = c(0, 0), y = c(effect("condition*AgeGroup", lm1)$lower[1], effect("condition*AgeGroup", lm1)$upper[1]), col = cols[3])
-lines(x = c(0.9, 0.9), y = c(effect("condition*AgeGroup", lm1)$lower[2], effect("condition*AgeGroup", lm1)$upper[2]), col = cols[3])
-lines(x = c(0.1, 0.1), y = c(effect("condition*AgeGroup", lm1)$lower[3], effect("condition*AgeGroup", lm1)$upper[3]), col = cols[2])
-lines(x = c(1, 1), y = c(effect("condition*AgeGroup", lm1)$lower[4], effect("condition*AgeGroup", lm1)$upper[4]), col = cols[2])
-legend("topleft", lty = 1, pch = 16, col = cols[3:2], legend = c("Younger", "Older"), bty = "n")
+lines(x = c(0, 0.9), y = effect("condition*AgeGroup", lm1)$fit[1:2], pch = 16, col = cols[3], type = "b", lwd = 1.5)
+lines(x = c(0.1, 1), y = effect("condition*AgeGroup", lm1)$fit[3:4], pch = 16, col = cols[2], type = "b", lwd = 1.5)
+lines(x = c(0, 0), y = c(effect("condition*AgeGroup", lm1)$lower[1], effect("condition*AgeGroup", lm1)$upper[1]), col = cols[3], lwd = 1.5)
+lines(x = c(0.9, 0.9), y = c(effect("condition*AgeGroup", lm1)$lower[2], effect("condition*AgeGroup", lm1)$upper[2]), col = cols[3], lwd = 1.5)
+lines(x = c(0.1, 0.1), y = c(effect("condition*AgeGroup", lm1)$lower[3], effect("condition*AgeGroup", lm1)$upper[3]), col = cols[2], lwd = 1.5)
+lines(x = c(1, 1), y = c(effect("condition*AgeGroup", lm1)$lower[4], effect("condition*AgeGroup", lm1)$upper[4]), col = cols[2], lwd = 1.5)
+legend("topleft", lty = 1, lwd = 1.5, pch = 16, col = cols[3:2], legend = c("Younger", "Older"), bty = "n")
 dev.off()
 
 # Model old and young separately due to interaction effect
@@ -154,6 +166,46 @@ lm2 <- lme(globalsignal_rms ~ condition * AgeGroup + FD, data = subset(data_glob
 summary(lm2)
 lm3 <- lme(globalsignal_rms ~ condition * AgeGroup + FD, data = subset(data_glob, data_glob$session %in% c(2, 4)), random = ~1|ID)
 summary(lm3)
+
+
+# Full model, global signal amplitude (SD, following Wong et al)
+lm2 <- lme(globalsignal_sd ~ condition * AgeGroup + FD, data = data_glob, random = ~1|ID)
+summary(lm2)
+intervals(lm2)
+plot(effect("condition*AgeGroup", lm2))
+
+pdf("GS2.pdf", height = 6, width = 6) 
+par(mar = c(4, 5, 1, 2))
+plot(1, frame.plot = F, xlim = c(0, 1), ylim = c(10, 22), xlab = "", ylab = "Global signal amplitude (sd)", xaxt = "n", type = "n")
+axis(1, at = c(0.05, 0.95), labels = c("Full sleep", "Sleep deprived"))
+lines(x = c(0, 0.9), y = effect("condition*AgeGroup", lm2)$fit[1:2], pch = 16, col = cols[3], type = "b", lwd = 1.5)
+lines(x = c(0.1, 1), y = effect("condition*AgeGroup", lm2)$fit[3:4], pch = 16, col = cols[2], type = "b", lwd = 1.5)
+lines(x = c(0, 0), y = c(effect("condition*AgeGroup", lm2)$lower[1], effect("condition*AgeGroup", lm2)$upper[1]), col = cols[3], lwd = 1.5)
+lines(x = c(0.9, 0.9), y = c(effect("condition*AgeGroup", lm2)$lower[2], effect("condition*AgeGroup", lm2)$upper[2]), col = cols[3], lwd = 1.5)
+lines(x = c(0.1, 0.1), y = c(effect("condition*AgeGroup", lm2)$lower[3], effect("condition*AgeGroup", lm2)$upper[3]), col = cols[2], lwd = 1.5)
+lines(x = c(1, 1), y = c(effect("condition*AgeGroup", lm2)$lower[4], effect("condition*AgeGroup", lm2)$upper[4]), col = cols[2], lwd = 1.5)
+legend("topleft", lty = 1, lwd = 1.5, pch = 16, col = cols[3:2], legend = c("Younger", "Older"), bty = "n")
+dev.off()
+
+# Full model, global signal amplitude (log SD to better meet model assumptions)
+lm3 <- lme(globalsignal_logsd ~ condition * AgeGroup + FD, data = data_glob, random = ~1|ID)
+summary(lm3)
+intervals(lm3)
+plot(effect("condition*AgeGroup", lm3))
+
+pdf("GS3.pdf", height = 6, width = 6) 
+par(mar = c(4, 5, 1, 2))
+plot(1, frame.plot = F, xlim = c(0, 1), ylim = c(2.3, 3.1), xlab = "", ylab = "Global signal amplitude (ln sd)", xaxt = "n", type = "n")
+axis(1, at = c(0.05, 0.95), labels = c("Full sleep", "Sleep deprived"))
+lines(x = c(0, 0.9), y = effect("condition*AgeGroup", lm3)$fit[1:2], pch = 16, col = cols[3], type = "b", lwd = 1.5)
+lines(x = c(0.1, 1), y = effect("condition*AgeGroup", lm3)$fit[3:4], pch = 16, col = cols[2], type = "b", lwd = 1.5)
+lines(x = c(0, 0), y = c(effect("condition*AgeGroup", lm3)$lower[1], effect("condition*AgeGroup", lm3)$upper[1]), col = cols[3], lwd = 1.5)
+lines(x = c(0.9, 0.9), y = c(effect("condition*AgeGroup", lm3)$lower[2], effect("condition*AgeGroup", lm3)$upper[2]), col = cols[3], lwd = 1.5)
+lines(x = c(0.1, 0.1), y = c(effect("condition*AgeGroup", lm3)$lower[3], effect("condition*AgeGroup", lm3)$upper[3]), col = cols[2], lwd = 1.5)
+lines(x = c(1, 1), y = c(effect("condition*AgeGroup", lm3)$lower[4], effect("condition*AgeGroup", lm3)$upper[4]), col = cols[2], lwd = 1.5)
+legend("topleft", lty = 1, lwd = 1.5, pch = 16, col = cols[3:2], legend = c("Younger", "Older"), bty = "n")
+dev.off()
+
 
 #######################
 
