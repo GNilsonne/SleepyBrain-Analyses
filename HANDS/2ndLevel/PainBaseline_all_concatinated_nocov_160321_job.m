a17de64ca4b6393ce8e5983223fdb537a5b06922
Gustav Nilsonne <gustav.nilsonne@ki.se>
2016-08-02 17:22:51 +0200

    Initial commit

---
+++ HANDS/2ndLevel/PainBaseline_all_concatinated_nocov_160321_job.m
@@ -0,0 +1,144 @@
+%-----------------------------------------------------------------------
+% Job saved on 21-Mar-2016 17:08:26 by cfg_util (rev $Rev: 6134 $)
+% spm SPM - SPM12 (6225)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/data/stress/HANDS_AGE/MainEffects/Painvsbaseline/Concatinated/All_nocovariates_160321'};
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+                                                          '/data/stress/HANDS_AGE/1st_level/002/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/015/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/037/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/038/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/040/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/043/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/049/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/45Slices/062_2/1st_level_150320/con_0001.nii,1'
+                                                          '/data/stress/HANDS_AGE/45Slices/070_1/1st_level_150320/con_0001.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/075/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/45Slices/079_1/1st_level_150320/con_0001.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/081/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/45Slices/082_1/1st_level_150320/con_0001.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/086/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/090/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/094/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/104/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/115/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/127/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/129/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/45Slices/137_2/1st_level_150320/con_0001.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/140/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/143/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/146/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/156/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/164/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/165/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/173/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/177/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/188/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/190/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/191/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/194/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/197/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/202/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/210/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/215/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/222/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/227/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/229/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/233/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/240/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/245/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/249/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/250/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/253/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/46Slices/263_1/1st_level_150320/con_0001.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/267/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/276/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/286/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/295/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/299/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/306/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/313/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/315/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/342/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/352/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/354/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/356/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/357/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/358/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/364/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/376/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/379/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/383/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/389/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/410/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/411/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/425/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/426/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/427/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/429/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/431/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/433/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/436/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/441/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/446/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/448/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/451/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/457/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/460/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/466/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/470/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/472/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/475/con_0003.nii,1'
+                                                          '/data/stress/HANDS_AGE/1st_level/497/con_0003.nii,1'
+                                                          };
+%%
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'Pain > baseline [all]';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 0;
+matlabbatch{4}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{4}.spm.stats.results.conspec(1).titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec(1).contrasts = 1;
+matlabbatch{4}.spm.stats.results.conspec(1).threshdesc = 'FWE';
+matlabbatch{4}.spm.stats.results.conspec(1).thresh = 0.05;
+matlabbatch{4}.spm.stats.results.conspec(1).extent = 0;
+matlabbatch{4}.spm.stats.results.conspec(1).mask.none = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec(2).contrasts = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).threshdesc = 'none';
+matlabbatch{4}.spm.stats.results.conspec(2).thresh = 0.001;
+matlabbatch{4}.spm.stats.results.conspec(2).extent = 0;
+matlabbatch{4}.spm.stats.results.conspec(2).mask.none = 1;
+matlabbatch{4}.spm.stats.results.units = 1;
+matlabbatch{4}.spm.stats.results.print = 'ps';
+matlabbatch{4}.spm.stats.results.write.none = 1;
+matlabbatch{5}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{5}.spm.stats.results.conspec(1).titlestr = '';
+matlabbatch{5}.spm.stats.results.conspec(1).contrasts = 1;
+matlabbatch{5}.spm.stats.results.conspec(1).threshdesc = 'FWE';
+matlabbatch{5}.spm.stats.results.conspec(1).thresh = 0.05;
+matlabbatch{5}.spm.stats.results.conspec(1).extent = 0;
+matlabbatch{5}.spm.stats.results.conspec(1).mask.none = 1;
+matlabbatch{5}.spm.stats.results.conspec(2).titlestr = '';
+matlabbatch{5}.spm.stats.results.conspec(2).contrasts = 1;
+matlabbatch{5}.spm.stats.results.conspec(2).threshdesc = 'none';
+matlabbatch{5}.spm.stats.results.conspec(2).thresh = 0.001;
+matlabbatch{5}.spm.stats.results.conspec(2).extent = 0;
+matlabbatch{5}.spm.stats.results.conspec(2).mask.none = 1;
+matlabbatch{5}.spm.stats.results.units = 1;
+matlabbatch{5}.spm.stats.results.print = 'csv';
+matlabbatch{5}.spm.stats.results.write.none = 1;
