14555f0d8fd0a62989e4ee14a80443e1fb5c030a
Gustav Nilsonne <gustav.nilsonne@ki.se>
2017-05-01 14:34:28 +0200

    Committing 30-component ICA

---
+++ Resting State/ICA_30components_2ndlevel/N6_masked/N6_170501_masked.m
@@ -0,0 +1,9 @@
+% List of open inputs
+nrun = X; % enter the number of runs here
+jobfile = {'/data/stress/Resting_State/2nd level networks 170419/N6_masked/N6_170501_masked_job.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
