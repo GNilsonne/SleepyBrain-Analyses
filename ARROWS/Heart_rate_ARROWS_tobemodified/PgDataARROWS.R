f29254cb0bee9c18e428617ea87406ab6ebc1e8f
Sandra Tamm <sandra.tamm@stressforskning.su.se>
2018-07-02 16:57:39 +0200

    Script to read pulse gating logfiles from ARROWS experiment

--- ARROWS/Heart_rate_ARROWS_tobemodified/PgDataARROWS.R
+++ ARROWS/Heart_rate_ARROWS_tobemodified/PgDataARROWS.R
@@ -1,6 +1,7 @@
 # Script to read pulse gating logfiles from ARROWS experiment
 # By Sandra Tamm
 # Modified by Gustav Nilsonne 2015-12-19
+# Adapted for ARROWS 2018-06-27
 
 setwd("~/Box Sync/Sleepy Brain/Datafiles/Pulse_gating_files")
 
@@ -19,16 +20,11 @@ PgDataARROWS$Subject <- as.integer(substr(PgDataARROWS$File, 1, 3))
 # Extract date
 PgDataARROWS$Date <- as.Date(as.character(substr(PgDataARROWS$File, 5, 10)), "%y%m%d")
 
-# List included subjects ### To be changed!
-IncludedSubjects <- read.table("../Subjects_140818.csv", sep=";", header=T)
-#IncludedSubjects <- as.integer(IncludedSubjects$CanBeIncludedForInterventionEffects)
-IncludedSubjects <- as.integer(IncludedSubjects$FulfillsCriteriaAndNoPathologicalFinding) # This is for the purpose of quality review.
+# List included subjects
+IncludedSubjects <- read.table("../SubjectsForARROWS.csv", sep=";", header=T)
 
-# May need to be used later. 352 was interupted and therefore data is incomplete
-#IncludedSubjects <-IncludedSubjects[IncludedSubjects != 352]
-# Subject 115 and 86 only have one full registration, therefore removed  
-#IncludedSubjects <-IncludedSubjects[IncludedSubjects != 115]
-#IncludedSubjects <-IncludedSubjects[IncludedSubjects != 086]
+IncludedSubjects <- as.integer(IncludedSubjects$CompletedScanning) # This is for the purpose of quality review.
+
 
 # Retain only subjects in list
 PgDataARROWS <- PgDataARROWS[PgDataARROWS$Subject %in% as.integer(IncludedSubjects), ]
@@ -39,5 +35,5 @@ plot(PgDataARROWS[ ,1], type="l", xlab = "Row", ylab = "Time (centiseconds)")
 # Check later that the correct participants are here!
 IncludedSubjectsPgARROWS <- unique(PgDataARROWS$Subject)
 
-write.csv2(PgDataARROWS, file = "../HR/PgDataARROWS_86subjects.csv", row.names=FALSE)
-write.csv2(IncludedSubjectsPgARROWS, file = "../HR/IncludedSubjectsPgARROWS_86subjects.csv", row.names=FALSE)
+write.csv2(PgDataARROWS, file = "../HR/PgDataARROWS.csv", row.names=FALSE)
+write.csv2(IncludedSubjectsPgARROWS, file = "../HR/IncludedSubjectsPgARROWS.csv", row.names=FALSE)
