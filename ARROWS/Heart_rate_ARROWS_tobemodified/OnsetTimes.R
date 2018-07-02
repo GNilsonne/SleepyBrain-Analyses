409030cef91604527637eea5af5a271547ad350d
Sandra Tamm <sandra.tamm@stressforskning.su.se>
2018-07-02 16:57:26 +0200

    Script to process pulse gating data from ARROWS experiment

--- ARROWS/Heart_rate_ARROWS_tobemodified/OnsetTimes.R
+++ ARROWS/Heart_rate_ARROWS_tobemodified/OnsetTimes.R
@@ -1,16 +1,17 @@
 # Script to process pulse gating data from HANDS experiment
 # By Sandra Tamm
 # Modified by Gustav Nilsonne 2015-12-19
+# Adapted for ARROWS experiment 2018-06-27
 
-setwd("C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles")
+setwd("~/Box Sync/Sleepy Brain/Datafiles")
 
 # List included subjects 
-IncludedSubjects <- read.table("HR/IncludedSubjectsPgHands_86subjects.csv", header=T, quote="\"")$x
+IncludedSubjects <- read.table("HR/IncludedSubjectsPgARROWS.csv", header=T, quote="\"")$x
 
 # Get onset times for stimuli 
 OnsetTimesForAll <- list()
-AllOnsetFiles <- list.files("Presentation_logfiles/", pattern = "HANDS_sce.log", recursive = T)
-AllStimulusFiles <- list.files("Presentation_logfiles/", pattern = "HANDS_log.txt", recursive = T)
+AllOnsetFiles <- list.files("Presentation_logfiles/", pattern = "ARROWS_sce.log", recursive = T)
+AllStimulusFiles <- list.files("Presentation_logfiles/", pattern = "ARROWS_log.txt", recursive = T)
 
 # Remove times for non-included subjects
 getSubjectFromFileName <- function (filename) {
@@ -20,6 +21,11 @@ getSubjectFromFileName <- function (filename) {
 AllOnsetFiles = AllOnsetFiles[unlist(lapply(AllOnsetFiles, getSubjectFromFileName)) %in% IncludedSubjects]
 AllStimulusFiles = AllStimulusFiles[unlist(lapply(AllStimulusFiles, getSubjectFromFileName)) %in% IncludedSubjects]
 
+## Needs to be checked with Presentation script/onset times in fMRI analysis
+
+#Check what is wrong with subject 190 and 426!!
+
+
 # Find onset times
 for(i in 1:length(AllOnsetFiles)){
   OnsetTime <- read.table(paste("Presentation_logfiles/",AllOnsetFiles[i], sep = ""), header=F, quote="\"", fill=TRUE, blank.lines.skip=FALSE)
@@ -28,21 +34,11 @@ for(i in 1:length(AllOnsetFiles)){
   OnsetTime$File <- AllOnsetFiles[i]
   OnsetTime$Subject <- as.integer(substr(OnsetTime$File, 1, 3)) 
   OnsetTime$Date <- as.integer(substr(OnsetTime$File, 5, 10))
-  OnsetTime <- subset(OnsetTime, V2 == "Pic2")
+  OnsetTime <- subset(OnsetTime, V2 == "Pic")
   OnsetTime$V4 <- as.numeric(as.character(OnsetTime$V4))/10000
-  if(i == 90){ # Ugly hack (credit to GN) to handle sessions with fewer than 40 events recorded
-    OnsetTime <- rbind(OnsetTime, rep(NA, 5))
-    OnsetTime <- rbind(OnsetTime, rep(NA, 5))
-  }
-  if(i == 111){
-    OnsetTime <- rbind(OnsetTime, rep(NA, 5))
-    OnsetTime <- rbind(OnsetTime, rep(NA, 5))
-    OnsetTime <- rbind(OnsetTime, rep(NA, 5))
-    OnsetTime <- rbind(OnsetTime, rep(NA, 5))
-  }
   StimulusType <- read.delim(paste("Presentation_logfiles/", AllStimulusFiles[i], sep = ""))
   OnsetTimeAndStimulus <- cbind(OnsetTime, StimulusType) 
-  OnsetTimeAndStimulus <- OnsetTimeAndStimulus[ , c("V4", "Condition", "Rated_Unpleasantness", "Subject", "Date")]
+  OnsetTimeAndStimulus <- OnsetTimeAndStimulus[ , c("V4", "StimulusType", "RatedSuccessOfRegulation", "Subject", "Date")]
   if(!is.na(OnsetTime$Subject[1]) && OnsetTime$Subject[1] %in% as.integer(IncludedSubjects) ){
     OnsetTimesForAll[[length(OnsetTimesForAll)+1]] <- OnsetTimeAndStimulus
   }
@@ -63,15 +59,17 @@ for(i in 1:length(OnsetTimesForAll)){
   }
 }
 
-write.csv2(OnsetTimesForAll, file = "HR/OnsetTimesForAll_86subjects.csv", row.names=FALSE)
+write.csv2(OnsetTimesForAll, file = "HR/OnsetTimesForAll_ARROWS.csv", row.names=FALSE)
 
 # Import pulse gating data
-PgDataHandsTime <- read.csv("HR/PgDataHandsTime_86subjects.csv", sep=";")
+PgDataARROWSTime <- read.csv("HR/PgDataARROWSTime.csv", sep=";")
 
 # Rescale time variable
-PgDataHandsTime[ , 1] <- as.numeric(PgDataHandsTime[ , 1])/100
+PgDataARROWSTime[ , 1] <- as.numeric(PgDataARROWSTime[ , 1])/100
 
-# Cut pieces of data that start 4 seconds before onset of every stimulus and end 10 seconds after
+
+####### Change?? for ARROWS
+# Cut pieces of data that start 6 seconds before onset of every stimulus (4 s before arrow) and end 16 seconds after
 # 4 seconds is chosen because that was the shortest possible duration of the jittered fixation cross
 # 10 seconds extends into the rating event and we are unlikely to be interested in anything going on later than that
 CutFun <- function(x){
@@ -79,15 +77,15 @@ CutFun <- function(x){
     return(rep(NA, 1401))
   } else {
     ThisOnset <- round(x, 2)
-    FirstRow <- (ThisOnset-4)*100
-    LastRow <- (ThisOnset+10)*100
+    FirstRow <- (ThisOnset-6)*100
+    LastRow <- (ThisOnset+16)*100
     #if (LastRow > length(PgDataHandsTime[,])) {
     #  stop(paste("Error in CutFun. x: ", x, " i: ", i, " LastRow: ", LastRow))
     #}
     #if (i > 62) stop(paste("iiii", i, "xxxx", x))
     colname = paste("X", Subject, "...", Session, sep="")
-    colindex = which(names(PgDataHandsTime) == colname)
-    CutBit <- PgDataHandsTime[FirstRow:LastRow, colindex] 
+    colindex = which(names(PgDataARROWSTime) == colname)
+    CutBit <- PgDataARROWSTime[FirstRow:LastRow, colindex] 
     return(CutBit)
   }
 }
@@ -99,6 +97,6 @@ for(i in 1:length(OnsetTimesForAll)){
   SuperList <- sapply(OnsetTimesForAll[[i]]$V4, CutFun)
   if(length(SuperList[[1]]) != 0){
     colnames(SuperList) <- OnsetTimesForAll[[i]]$Condition
-    write.csv2(SuperList, file = paste("HR/PgDataHandsStimulus/", Subject, "_", Session, ".csv", sep=""), row.names=FALSE)
+    write.csv2(SuperList, file = paste("HR/PgDataARROWSStimulus/", Subject, "_", Session, ".csv", sep=""), row.names=FALSE)
   }
 }
