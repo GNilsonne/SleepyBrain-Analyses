dd95f06c5df34a632f69cdf0973fa55e3c19195b
Sandra Tamm <sandra.tamm@stressforskning.su.se>
2018-08-22 08:23:33 +0200

    Full models of heart rate

--- FACES/Heart_rate_Faces/MEModel.R
+++ FACES/Heart_rate_Faces/MEModel.R
@@ -69,18 +69,29 @@ for(i in 1:length(Files)){
   }
 }
 
-# How much data was imputed.
+# How much data was imputed. Check for FACES!
 n_na
-n_na/(60*2201*length(Files))
+n_na/(60*2401*length(Files))
 n_na2
-n_na2/(60*2201*length(Files))
+n_na2/(60*2401*length(Files))
 
 # Plot time courses
-meantimecourseMaintainNeutral <- rowMeans(timecoursesMaintainNeutral[, -1], na.rm = T)
-meantimecourseMaintainNegative <- rowMeans(timecoursesMaintainNegative[, -1], na.rm = T)
-meantimecourseDownregulateNegative <- rowMeans(timecoursesDownregulateNegative[, -1], na.rm = T)
-meantimecourseUpregulateNegative <- rowMeans(timecoursesUpregulateNegative[, -1], na.rm = T)
- 
+meantimecourseNeutral <- rowMeans(timecoursesNeutral[, -1], na.rm = T)
+meantimecourseHappy <- rowMeans(timecoursesHappy[, -1], na.rm = T)
+meantimecourseAngry <- rowMeans(timecoursesAngry[, -1], na.rm = T)
+
+setwd("~/Desktop/SleepyBrain-Analyses/FACES/Heart_rate_Faces")
+pdf("heart_rate.pdf")
+plot(meantimecourseNeutral, type = "n", frame.plot = F, xlab = "time", ylab = "Heart rate", xaxt = "n", ylim = c(66, 70))
+polygon(x = c(200, 200, 600, 600), y = c(2, 0, 0, 2), density = NULL, border = NULL, col = "gray", lty = 4)
+axis(1, at = c(0, 200, 2200, 2400), labels = c(" ","start", "end", ""))
+lines(lowess(meantimecourseNeutral, f = 2/5), col = "blue", lwd = 2)
+lines(lowess(meantimecourseAngry, f = 2/5), col = "red", lwd = 2, lty = 2)
+lines(lowess(meantimecourseHappy, f = 2/5), col = "green", lwd = 2, lty = 3)
+legend("topright", lty = c(1, 2), lwd = 2, col = c("blue", "red", "green"), 
+       legend = c("Neutral", "Angry", "Happy"), bty = "n")
+
+
 # Take out time courses for full sleep/psd
 sessions <- data.frame(subject = as.integer(substr(filenames, 1, 3)), session = substr(filenames, 5, 5))
 sessions <- sessions[sessions$subject %in% IncludedSubjects, ]
@@ -99,98 +110,55 @@ timecourses_psd <- timecourses_all[, psd_index]
 meantimecoursefullsleep <- rowMeans(timecourses_fullsleep, na.rm = T)
 meantimecoursepsd <- rowMeans(timecourses_psd, na.rm = T)
 
-# # Plot aggregate time courses
-# # This is the plot that goes into the data descriptor manuscript
-# meantimecourse <- (meantimecoursepain + meantimecoursenopain)/2 # Events are balanced so this should be all right
-# pdf(file = "C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles/HR/agg_timecourse.pdf")
-# plot(meantimecourse, type = "n", frame.plot = F, xlab = "time, s", ylab = "Heart rate, index", xaxt = "n", yaxt = "n")
-# polygon(x = c(400, 400, 750, 750), y = c(2, 0, 0, 2), density = NULL, border = NULL, col = "gray", lty = 0)
-# abline(v = 950, lty = 2)
-# axis(1, at = c(0, 400, 750, 950, 1400), labels = c(-4, 0, 3.5, 5.5, 10))
-# axis(2, at = c(0.997, 1, 1.003, 1.006), labels = c(0.997, 1, 1.003, 1.006))
-# lines(meantimecourse)
-# dev.off()
-# #lines(lowess(meantimecourse, f = 1/5), col = "blue", lwd = 2)
+plot(meantimecoursefullsleep, type = "n", frame.plot = F, xlab = "time", ylab = "Heart rate", xaxt = "n", ylim = c(66, 70))
+axis(1, at = c(0, 200, 2200, 2400), labels = c(" ","start", "end", ""))
+lines(lowess(meantimecoursefullsleep, f = 2/5), col = "blue", lwd = 2)
+lines(lowess(meantimecoursepsd, f = 2/5), col = "red", lwd = 2, lty = 2)
+legend("topright", lty = c(1, 2), lwd = 2, col = c("blue", "red"), legend = c("Full sleep", "Sleep deprived"), bty = "n")
+
+
 
 # Plot means for events
 includedfilenames <- filenames[as.integer(substr(filenames, 1, 3)) %in% IncludedSubjects]
-MaintainNeutralevents$subject <- as.integer(substr(includedfilenames, 1, 3))
-MaintainNegativeevents$subject <- as.integer(substr(includedfilenames, 1, 3))
-DownregulateNegativeevents$subject <- as.integer(substr(includedfilenames, 1, 3))
-UpregulateNegativeevents$subject <- as.integer(substr(includedfilenames, 1, 3))
-MaintainNeutralevents$session <- as.integer(substr(includedfilenames, 5, 5))
-MaintainNegativeevents$session <- as.integer(substr(includedfilenames, 5, 5))
-DownregulateNegativeevents$session <- as.integer(substr(includedfilenames, 5, 5))
-UpregulateNegativeevents$session <- as.integer(substr(includedfilenames, 5, 5))
-MaintainNeutralevents <- melt(MaintainNeutralevents, id.vars = c("subject", "session"))
-MaintainNeutralevents$stimulus <- "MaintainNeutral"
-MaintainNegativeevents <- melt(MaintainNegativeevents, id.vars = c("subject", "session"))
-MaintainNegativeevents$stimulus <- "MaintainNegative"
-DownregulateNegativeevents <- melt(DownregulateNegativeevents, id.vars = c("subject", "session"))
-DownregulateNegativeevents$stimulus <- "DownregulateNegative"
-UpregulateNegativeevents <- melt(UpregulateNegativeevents, id.vars = c("subject", "session"))
-UpregulateNegativeevents$stimulus <- "UpregulateNegative"
-events <- rbind(MaintainNeutralevents, MaintainNegativeevents, DownregulateNegativeevents, UpregulateNegativeevents)
-boxplot(value ~ stimulus, data = events)
+NeutralBlocks$subject <- as.integer(substr(includedfilenames, 1, 3))
+AngryBlocks$subject <- as.integer(substr(includedfilenames, 1, 3))
+HappyBlocks$subject <- as.integer(substr(includedfilenames, 1, 3))
+NeutralBlocks$session <- as.integer(substr(includedfilenames, 5, 5))
+AngryBlocks$session <- as.integer(substr(includedfilenames, 5, 5))
+HappyBlocks$session <- as.integer(substr(includedfilenames, 5, 5))
+NeutralBlocks <- melt(NeutralBlocks, id.vars = c("subject", "session"))
+NeutralBlocks$stimulus <- "Neutral"
+AngryBlocks <- melt(AngryBlocks, id.vars = c("subject", "session"))
+AngryBlocks$stimulus <- "Angry"
+HappyBlocks <- melt(HappyBlocks, id.vars = c("subject", "session"))
+HappyBlocks$stimulus <- "Happy"
+blocks <- rbind(NeutralBlocks, AngryBlocks, HappyBlocks)
+boxplot(value ~ stimulus, data = blocks)
+dev.off()
 
 # Analyse data
 demographics <- read.csv("~/Box Sync/Sleepy Brain/Datafiles/150215_Demographic.csv", sep=";", dec=",")
-events <- merge(events, subset(demographics, select = c(Subject, AgeGroup)), by.x = "subject", by.y = "Subject")
+blocks <- merge(blocks, subset(demographics, select = c(Subject, AgeGroup)), by.x = "subject", by.y = "Subject")
 randomisation <- read.csv("~/Box Sync/Sleepy Brain/Datafiles/RandomisationList_140804.csv", sep=";")
-events <- merge(events, randomisation, by.x = "subject", by.y = "Subject")
-events$condition <- "fullsleep"
-events$condition[events$Sl_cond == 1 & events$session == 1] <- "psd"
-events$condition[events$Sl_cond == 2 & events$session == 2] <- "psd"
-events$condition <- as.factor(events$condition)
-events$AgeGroup <- relevel(events$AgeGroup, ref = "Young")
-events$condition <- relevel(events$condition, ref = "fullsleep")
+blocks <- merge(blocks, randomisation, by.x = "subject", by.y = "Subject")
+blocks$condition <- "fullsleep"
+blocks$condition[blocks$Sl_cond == 1 & blocks$session == 1] <- "psd"
+blocks$condition[blocks$Sl_cond == 2 & blocks$session == 2] <- "psd"
+blocks$condition <- as.factor(blocks$condition)
+blocks$AgeGroup <- relevel(blocks$AgeGroup, ref = "Young")
+blocks$condition <- relevel(blocks$condition, ref = "fullsleep")
 
 
 # Model for purpose of hypothesis testing
-events$stimulus <- as.factor(events$stimulus)
-events$stimulus <- relevel(events$stimulus, ref = "MaintainNeutral")
-lme2 <- lme(value ~ stimulus*condition*AgeGroup, data = events, random = ~ 1|subject/session, na.action = na.omit)
+blocks$stimulus <- as.factor(blocks$stimulus)
+blocks$stimulus <- relevel(blocks$stimulus, ref = "Neutral")
+blocks$AgeGroup <- relevel(blocks$AgeGroup, ref = "Young")
+lme2 <- lme(value ~ stimulus*condition*AgeGroup, data = blocks, random = ~ 1|subject/session, na.action = na.omit)
 summary(lme2)
 intervals(lme2)
-setwd("/Users/santam/Box Sync/Sleepy Brain/Datafiles/HR")
+setwd("/Users/santam/Desktop/SleepyBrain-Analyses/FACES/Heart_rate_Faces/")
 write.csv2(summary(lme2)$tTable, file = "Full_model.csv")
 write.csv2(intervals(lme2)$fixed, file = "Full_model_intervals.csv")
 
-
-# Model without maintain neutral
-eventsNegative <- subset(events, stimulus != "MaintainNeutral")
-eventsNegative$stimulus <- droplevels(eventsNegative)$stimulus
-eventsNegative$stimulus <- relevel(eventsNegative$stimulus, ref = "MaintainNegative")
-lme2 <- lme(value ~ stimulus*condition*AgeGroup, data = eventsNegative, random = ~ 1|subject/session, na.action = na.omit)
-summary(lme2)
-intervals(lme2)
-
-# Plot time courses for event
-plot(meantimecourseUpregulateNegative, type = "n", frame.plot = F, xlab = "", ylab = "Heart rate, index", xaxt = "n", ylim = c(0.97,1.02))
-polygon(x = c(400, 400, 600, 600), y = c(2, 0, 0, 2), density = NULL, border = NULL, col = "#EEEEEE", lty = 0)
-polygon(x = c(600, 600, 1100, 1100), y = c(2, 0, 0, 2), density = NULL, border = NULL, col = "#FFEEEE", lty = 0)
-abline(v = 1300, lty = 2)
-axis(1, at = c(0, 400, 600, 1100, 1300, 1600), labels = c(" ", "Arrow", "Picture", "Blank", "Rating", " "))
-#lines(meantimecourseMaintainNegative, col = "#256EFF")
-#lines(meantimecourseMaintainNeutral, col = "#46237A")
-#lines(meantimecourseDownregulateNegative, col = "#3DDC97")
-#lines(meantimecourseUpregulateNegative, col = "#FF495C")
-lines(lowess(meantimecourseMaintainNegative, f = 1/5), col = "#256EFF", lwd = 2)
-lines(lowess(meantimecourseMaintainNeutral, f = 1/5), col = "#46237A", lwd = 2)
-lines(lowess(meantimecourseDownregulateNegative, f = 1/5), col = "#3DDC97", lwd = 2)
-lines(lowess(meantimecourseUpregulateNegative, f = 1/5), col = "#FF495C", lwd = 2)
-legend("topleft", lty = 1, lwd = 2, col = c("#256EFF", "#46237A", "#3DDC97", "#FF495C"), 
-       legend = c("Maintain Negative", "Maintain Neutral", "Downregulate Negative", "Upregulate Negative"), bty = "n")
-
-
-# Plots for effect of sleep restriction
-plot(meantimecoursefullsleep, type = "n", frame.plot = F, xlab = "time", ylab = "Heart rate, index", xaxt = "n", ylim = c(0.98, 1.02))
-polygon(x = c(400, 400, 600, 600), y = c(2, 0, 0, 2), density = NULL, border = NULL, col = "gray", lty = 0)
-polygon(x = c(600, 600, 1100, 1100), y = c(2, 0, 0, 2), density = NULL, border = NULL, col = "#FFCCCC", lty = 0)
-abline(v = 1300, lty = 2)
-axis(1, at = c(0, 400, 600, 1100, 1300, 1600), labels = c(" ", "Arrow", "Picture", "Blank", "Rating", " "))
-lines(lowess(meantimecoursefullsleep, f = 2/5), col = "blue", lwd = 2)
-lines(lowess(meantimecoursepsd, f = 2/5), col = "red", lwd = 2, lty = 2)
-legend("topleft", lty = c(1, 2), lwd = 2, col = c("blue", "red"), legend = c("Full sleep", "Sleep deprived"), bty = "n")
 
 
