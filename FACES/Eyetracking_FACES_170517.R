8c5422869c9be5f04932c339c519f626068d889d
Sandra Tamm <sandra.tamm@stressforskning.su.se>
2018-09-03 15:42:08 +0200

    Include correct participants

--- FACES/Eyetracking_FACES_170517.R
+++ FACES/Eyetracking_FACES_170517.R
@@ -37,11 +37,9 @@ ViewpointDataFaces$Date <- as.Date(as.character(substr(ViewpointDataFaces$filena
 
 # List included subjects 
 IncludedSubjects <- read.table("../Subjects_151215.csv", sep=";", header=T)
-#IncludedSubjects <- as.integer(IncludedSubjects$CanBeIncludedForInterventionEffects)
-IncludedSubjects <- as.integer(IncludedSubjects$FulfillsCriteriaAndNoPathologicalFinding) # This is for the purpose of quality review.
+IncludedSubjects <- as.integer(IncludedSubjects$CanBeIncludedForInterventionEffects)
+#IncludedSubjects <- as.integer(IncludedSubjects$FulfillsCriteriaAndNoPathologicalFinding) # This is for the purpose of quality review.
 
-# May need to be used later to exclude additional subjects
-#IncludedSubjects <-IncludedSubjects[IncludedSubjects != 352]
 
 # Retain only subjects in list
 ViewpointDataFaces <- ViewpointDataFaces[ViewpointDataFaces$Subject %in% as.integer(IncludedSubjects), ]
@@ -54,9 +52,8 @@ length(unique(ViewpointDataFaces$Subject))
 
 
 write.csv2(ViewpointDataFaces, file = "../Eyetracking_FACES/ViepointDataFaces.csv", row.names=FALSE)
-write.csv2(IncludedSubjectsViewpointFaces, file = "../IncludedSubjectsViewpointFACES.csv", row.names=FALSE)
-#IncludedSubjectsViewpointFaces <- as.integer(read.csv2("../IncludedSubjectsViewpointFACES.csv")$x)
-#Read file
+write.csv2(IncludedSubjectsViewpointFaces, file = "../IncludedSubjectsViewpointFaces.csv", row.names=FALSE)
+
 
 
 # Get onset times for stimuli 
@@ -144,8 +141,10 @@ setwd("~/Box Sync/Sleepy Brain/Datafiles/Viewpoint_FACES_corrected/")
 data_out <- NULL
 onsets <- vector()
 indices <- 1:length(OnsetTimesForAll)
-indices <- indices[c(-15, -28, -35, -36, -54, -60, -69, -71, -79, -83, -97, -100, -103, -104, -105, -113, -114,
-                     -121, -124, -125, -131, -150, -154)] # Do not read when file is nonexistent or cannot be processed
+# Some of these files exist, but I can't see why they don't work
+indices <- indices[c(-19, -24, -25, -39, -43, -52, -54, -60, -64, -83, -84, -97, -100, 
+                     -107)] 
+
 for(i in indices){
   temp <- OnsetTimesForAll[[i]]
   subject <- temp$Subject[1]
@@ -179,19 +178,20 @@ for(i in indices){
   data$width[data$width > 0.3] <- NA
   data$height[data$height > 0.3] <- NA
 
-  width_lo001 <- loess(data$width ~ data$time, span = 0.01)
-  height_lo001 <- loess(data$height ~ data$time, span = 0.01)
-  data$width_lo001 <- predict(width_lo001, data$time)
-  data$height_lo001 <- predict(height_lo001, data$time)
-  width_lo005 <- loess(data$width ~ data$time, span = 0.05)
-  height_lo005 <- loess(data$height ~ data$time, span = 0.05)
-  data$width_lo005 <- predict(width_lo005, data$time)
-  data$height_lo005 <- predict(height_lo005, data$time)
-  
   # Remove participants weith less data than 50 %
   if(sum(is.na(data$width)) > 0.5*length(data$width) | sum(is.na(data$height)) > 0.5*length(data$height)){
     
   }else{
+    width_lo001 <- loess(data$width ~ data$time, span = 0.01)
+    height_lo001 <- loess(data$height ~ data$time, span = 0.01)
+    data$width_lo001 <- predict(width_lo001, data$time)
+    data$height_lo001 <- predict(height_lo001, data$time)
+    width_lo005 <- loess(data$width ~ data$time, span = 0.05)
+    height_lo005 <- loess(data$height ~ data$time, span = 0.05)
+    data$width_lo005 <- predict(width_lo005, data$time)
+    data$height_lo005 <- predict(height_lo005, data$time)
+  
+
     # Plot 
     plot(data$width ~ data$time, type = "l", main = paste(c(subject, session)))
     lines(data$width_lo001 ~ data$time, type = "l", col = "red", lwd = 2)
@@ -279,7 +279,7 @@ dataout_happy_agg <- aggregate(cbind(height, width) ~ index, data = dataout_agg_
 dataout_happy_agg$diameter <- (dataout_happy_agg$width + dataout_happy_agg$height)/2
 
 
-plot(diameter ~ index, data = dataout_neutral_agg, type = "l", frame.plot = F, xaxt = "n", main = paste("Diameter"), ylim = c(0.15, 0.25), xlab = "Time, s", ylab = "Diameter, cm", col = "blue")
+plot(diameter ~ index, data = dataout_neutral_agg, type = "l", frame.plot = F, xaxt = "n", main = paste("Diameter"), ylim = c(0.15, 0.23), xlab = "20 s block", ylab = "Diameter, cm", col = "blue")
 axis(1, at = c(0, 20, 220, 240), labels = c("", "start", "end", ""))
 lines(diameter ~ index, data = dataout_angry_agg, col = "red")
 lines(diameter ~ index, data = dataout_happy_agg, col = "green")
@@ -314,7 +314,7 @@ anyNA(dataout_agg_session)
 dataout_agg_fullsleep <- aggregate(diameter ~ index, data = dataout_agg_session[dataout_agg_session$condition == "FullSleep", ], FUN = "mean")
 dataout_agg_sleepdeprived <- aggregate(diameter ~ index, data = dataout_agg_session[dataout_agg_session$condition == "SleepDeprived", ], FUN = "mean")
 
-plot(diameter ~ index, data = dataout_agg_fullsleep, type = "l", frame.plot = F, xaxt = "n", main = paste("Pupil diameter"), ylim = c(0.17, 0.20), xlab = "Time, s", ylab = "Diameter, cm", col = "darkgreen")
+plot(diameter ~ index, data = dataout_agg_fullsleep, type = "l", frame.plot = F, xaxt = "n", main = paste("Pupil diameter"), ylim = c(0.15, 0.23), xlab = "20 s block", ylab = "Diameter, cm", col = "darkgreen")
 axis(1, at = c(0, 20, 220, 240), labels = c("", "start", "end", ""))
 lines(diameter ~ index, data = dataout_agg_sleepdeprived, col = "orange")
 legend("topleft", lty = 1, legend = c("Full sleep", "Sleep deprived"), col = c("darkgreen", "orange"), bty = "n")
@@ -331,43 +331,44 @@ while(anyNA(data_out$width)){
   data_out$width[impute_width] <- data_out$width[impute_width - 1]
 }
 data_out$diameter <- (data_out$height + data_out$width)/2
-data_delta <- NULL
-for (i in unique(data_out$subject)){
-  for (j in unique(data_out$event_no[data_out$subject == i])){
-    cutout3 <- data_out[data_out$subject == i & data_out$event_no == j, ]
-    cutout3$delta_diameter <- cutout3$diameter - mean(cutout3$diameter[1:20])
-    if(exists("data_delta") == FALSE){
-      data_delta <- cutout3
-    } else {
-      data_delta <- rbind(data_delta, cutout3)
-    }
-  }
-}
+#data_delta <- NULL
+# for (i in unique(data_out$subject)){
+#   for (j in unique(data_out$event_no[data_out$subject == i])){
+#     cutout3 <- data_out[data_out$subject == i & data_out$event_no == j, ]
+#     #???
+#     cutout3$delta_diameter <- cutout3$diameter #- mean(cutout3$diameter[1:20])
+#     if(exists("data_delta") == FALSE){
+#       data_delta <- cutout3
+#     } else {
+#       data_delta <- rbind(data_delta, cutout3)
+#     }
+#   }
+# }
 
-# Check plot
-data_delta_agg <- NULL
-for (i in unique(data_delta$subject)){
-  cutout4 <- data_delta[data_delta$subject == i, ]
-  cutout4_agg <- aggregate(delta_diameter ~ index, data = cutout4, FUN = "mean")
-  if(exists("data_delta_agg") == FALSE){
-    data_delta_agg <- cutout4_agg
-  } else {
-    data_delta_agg <- rbind(data_delta_agg, cutout4_agg)
-  }
-}
-data_delta_agg_agg <- aggregate(delta_diameter ~ index, data = data_delta_agg, FUN = "mean")
-plot(delta_diameter ~ index, data = data_delta_agg_agg, type = "l", frame.plot = F, xaxt = "n", main = paste("Diameter"), xlab = "Time, s", ylab = "Delta diameter, cm")
-axis(1, at = c(0, 20, 220, 240), labels = c("", "start", "end", ""))
-
+# # Check plot
+# data_delta_agg <- NULL
+# for (i in unique(data_delta$subject)){
+#   cutout4 <- data_delta[data_delta$subject == i, ]
+#   cutout4_agg <- aggregate(delta_diameter ~ index, data = cutout4, FUN = "mean")
+#   if(exists("data_delta_agg") == FALSE){
+#     data_delta_agg <- cutout4_agg
+#   } else {
+#     data_delta_agg <- rbind(data_delta_agg, cutout4_agg)
+#   }
+# }
+# data_delta_agg_agg <- aggregate(delta_diameter ~ index, data = data_delta_agg, FUN = "mean")
+# plot(delta_diameter ~ index, data = data_delta_agg_agg, type = "l", frame.plot = F, xaxt = "n", main = paste("Diameter"), xlab = "Time, s", ylab = "Delta diameter, cm")
+# axis(1, at = c(0, 20, 220, 240), labels = c("", "start", "end", ""))
+# 
 
 # Make data frame with mean responses for each event
 data_eventmeans <- NULL
-for (i in unique(data_delta$subject)){
-  for (j in unique(data_delta$event_no[data_delta$subject == i])){
-    cutout5 <- data_delta[data_delta$subject == i & data_delta$event_no == j, ]
+for (i in unique(data_out$subject)){
+  for (j in unique(data_out$event_no[data_out$subject == i])){
+    cutout5 <- data_out[data_out$subject == i & data_out$event_no == j, ]
     eventmeans <- data.frame(subject = cutout5$subject[1], date = cutout5$date[1], 
                              event_no <- cutout5$event_no[1], stimulus <- cutout5$stimulus[1],
-                             mean_event <- mean(cutout5$delta_diameter[21:60]), mean_postevent <- mean(cutout5$delta_diameter[61:80]))
+                             mean_event <- mean(cutout5$diameter[21:60]))
     if(exists("data_eventmeans") == FALSE){
       data_eventmeans <- eventmeans
     } else {
@@ -375,14 +376,15 @@ for (i in unique(data_delta$subject)){
     }
   }
 }
-names(data_eventmeans) <- c("subject", "date", "event_no", "stimulus", "mean_event", "mean_postevent")
+names(data_eventmeans) <- c("subject", "date", "event_no", "stimulus", "mean_event")
+# Removes NA caused by last registrations that are not complete
+data_eventmeans <- subset(data_eventmeans, !is.na(mean_event))
 
 data_eventmeans2 <- merge(data_eventmeans, demographics[, c("Subject", "AgeGroup", "Sex", "DateSession1", "DateSession2")], by.x = "subject", by.y = "Subject")
 data_eventmeans2 <- merge(data_eventmeans2, randomisation[, c("Subject", "Sl_cond")], by.x = "subject", by.y = "Subject")
 data_eventmeans2$session <- NA
 data_eventmeans2$session[data_eventmeans2$date == data_eventmeans2$DateSession1] <- 1
 data_eventmeans2$session[data_eventmeans2$date == data_eventmeans2$DateSession2] <- 2
-# Does not!
 anyNA(data_eventmeans2) # Should evaluate to FALSE
 data_eventmeans2$condition <- NA
 data_eventmeans2$condition[data_eventmeans2$session == 1 & data_eventmeans2$Sl_cond == 1] <- "SleepDeprived"
@@ -393,9 +395,6 @@ anyNA(data_eventmeans2)
 data_eventmeans2$condition <- relevel(as.factor(data_eventmeans2$condition), ref = "FullSleep")
 data_eventmeans2$AgeGroup <- relevel(as.factor(data_eventmeans2$AgeGroup), ref = "Young")
 
-# Find out how many responses have been rejected. Check for FACES!
-length(data_eventmeans2$event_no)
-length(data_eventmeans2$event_no)/(166*40)
 
 # Contrast coding
 contrasts(data_eventmeans2$AgeGroup) <- rbind(-.5, .5)
@@ -407,112 +406,20 @@ colnames(contrasts(data_eventmeans2$condition)) <- levels(data_eventmeans2$condi
 data_eventmeans2$stimulus <- droplevels(data_eventmeans2$stimulus)
 data_eventmeans2$stimulus <- relevel(data_eventmeans2$stimulus, ref = "neutral")
 
-# Build models
-# First model is without stimulus and sleep condition for purpose of technical validation
-#lme1 <- lme(mean_event ~ 1, data = data_eventmeans2, random = ~ 1|subject/session, na.action = na.omit)
-#summary(lme1)
-#intervals(lme1)
 
 lme2 <- lme(mean_event ~ stimulus*condition, data = data_eventmeans2, random = ~ 1|subject, na.action = na.omit)
 summary(lme2)
 intervals_lme2 <- intervals(lme2)
 plot(effect("stimulus*condition", lme2))
 setwd("~/Desktop/SleepyBrain-Analyses/FACES/Eyetracking/")
-write.csv(summary(lme2)$tTable, file = "Reduced_model.csv")
-write.csv(intervals_lme2$fixed, file = "Reduced_model_intervals.csv")
+write.csv2(summary(lme2)$tTable, file = "Reduced_model.csv")
+write.csv2(intervals_lme2$fixed, file = "Reduced_model_intervals.csv")
 
-lme3 <- lme(mean_postevent ~ stimulus*condition*AgeGroup, data = data_eventmeans2, random = ~ 1|subject, na.action = na.omit)
+lme3 <- lme(mean_event ~ stimulus*condition*AgeGroup, data = data_eventmeans2, random = ~ 1|subject, na.action = na.omit)
 summary(lme3)
 intervals_lme3 <- intervals(lme3)
 plot(effect("stimulus*condition*AgeGroup", lme3))
-write.csv(summary(lme3)$tTable, file = "Full_model.csv")
-write.csv(intervals_lme3$fixed, file = "Full_model_intervals.csv")
-
-##################
-
-# Make plots for publication
-data_delta_pain_agg <- NULL
-data_delta_nopain_agg <- NULL
-for (i in unique(data_delta$subject)){
-  cutout6 <- data_delta[data_delta$subject == i & data_delta$stimulus == "Pain", ]
-  cutout6_agg <- aggregate(delta_diameter ~ index, data = cutout6, FUN = "mean")
-  if(exists("data_delta_pain_agg") == FALSE){
-    data_delta_pain_agg <- cutout6_agg
-  } else {
-    data_delta_pain_agg <- rbind(data_delta_pain_agg, cutout6_agg)
-  }
-  cutout7 <- data_delta[data_delta$subject == i & data_delta$stimulus == "No_Pain", ]
-  cutout7_agg <- aggregate(delta_diameter ~ index, data = cutout7, FUN = "mean")
-  if(exists("data_delta_nopain_agg") == FALSE){
-    data_delta_nopain_agg <- cutout7_agg
-  } else {
-    data_delta_nopain_agg <- rbind(data_delta_nopain_agg, cutout7_agg)
-  }
-}
-data_delta_pain_agg_agg <- aggregate(delta_diameter ~ index, data = data_delta_pain_agg, FUN = "mean")
-data_delta_nopain_agg_agg <- aggregate(delta_diameter ~ index, data = data_delta_nopain_agg, FUN = "mean")
-data_delta_pain_agg_agg$delta_diameter_mm <- data_delta_pain_agg_agg$delta_diameter*10
-data_delta_nopain_agg_agg$delta_diameter_mm <- data_delta_nopain_agg_agg$delta_diameter*10
-
-data_delta <- merge(data_delta, demographics[, c("Subject", "DateSession1", "DateSession2")], by.x = "subject", by.y = "Subject")
-data_delta <- merge(data_delta, randomisation[, c("Subject", "Sl_cond")], by.x = "subject", by.y = "Subject")
-data_delta$session <- NA
-data_delta$session[data_delta$date == data_delta$DateSession1] <- 1
-data_delta$session[data_delta$date == data_delta$DateSession2] <- 2
-anyNA(data_delta) # Should evaluate to FALSE
-data_delta$condition <- NA
-data_delta$condition[data_delta$session == 1 & data_delta$Sl_cond == 1] <- "SleepDeprived"
-data_delta$condition[data_delta$session == 1 & data_delta$Sl_cond == 2] <- "FullSleep"
-data_delta$condition[data_delta$session == 2 & data_delta$Sl_cond == 2] <- "SleepDeprived"
-data_delta$condition[data_delta$session == 2 & data_delta$Sl_cond == 1] <- "FullSleep"
-anyNA(data_delta)
-data_delta$condition <- as.factor(data_delta$condition)
-
-data_delta_fullsleep_agg <- NULL
-data_delta_sleepdeprived_agg <- NULL
-for (i in unique(data_delta$subject)){
-  if(any(data_delta[data_delta$subject == i, ]$condition == "FullSleep")){
-    cutout8 <- data_delta[data_delta$subject == i & data_delta$condition == "FullSleep", ]
-    cutout8_agg <- aggregate(delta_diameter ~ index, data = cutout8, FUN = "mean")
-    if(exists("data_delta_fullsleep_agg") == FALSE){
-      data_delta_fullsleep_agg <- cutout8_agg
-    } else {
-      data_delta_fullsleep_agg <- rbind(data_delta_fullsleep_agg, cutout8_agg)
-    }
-  }
-  if(any(data_delta[data_delta$subject == i, ]$condition == "SleepDeprived")){
-    cutout9 <- data_delta[data_delta$subject == i & data_delta$condition == "SleepDeprived", ]
-    cutout9_agg <- aggregate(delta_diameter ~ index, data = cutout9, FUN = "mean")    
-    if(exists("data_delta_sleepdeprived_agg") == FALSE){
-      data_delta_sleepdeprived_agg <- cutout9_agg
-    } else {
-      data_delta_sleepdeprived_agg <- rbind(data_delta_sleepdeprived_agg, cutout9_agg)
-    }
-  }
-}
-
-data_delta_sleepdeprived_agg_agg <- aggregate(delta_diameter ~ index, data = data_delta_sleepdeprived_agg, FUN = "mean")
-data_delta_fullsleep_agg_agg <- aggregate(delta_diameter ~ index, data = data_delta_fullsleep_agg, FUN = "mean")
-data_delta_sleepdeprived_agg_agg$delta_diameter_mm <- data_delta_sleepdeprived_agg_agg$delta_diameter*10
-data_delta_fullsleep_agg_agg$delta_diameter_mm <- data_delta_fullsleep_agg_agg$delta_diameter*10
+write.csv2(summary(lme3)$tTable, file = "Full_model.csv")
+write.csv2(intervals_lme3$fixed, file = "Full_model_intervals.csv")
 
 
-pdf(file = "C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles/Eyetracking/agg_timecourse3.pdf")
-plot(delta_diameter_mm ~ index, data = data_delta_pain_agg_agg, type = "n", frame.plot = F, xaxt = "n", yaxt = "n", ylim = c(-0.25, 0.05), main = "", xlab = "Time, s", ylab = "Diameter change, mm")
-abline(v = 95, lty = 2)
-polygon(x = c(40, 40, 75, 75), y = c(1, -1, -1, 1), col = "gray", border = "gray")
-axis(1, at = c(0, 40, 75, 95, 140), labels = c(-4, 0, 3.5, 5.5, 10))
-axis(2, at = c(-0.25, -0.15, -0.05, 0.05))
-lines(delta_diameter_mm ~ index, data = data_delta_nopain_agg_agg, col = "blue", lwd = 2)
-lines(delta_diameter_mm ~ index, data = data_delta_pain_agg_agg, col = "red", lty = 2, lwd = 2)
-legend("bottomright", lty = c(1, 2), col = c("blue", "red"), legend = c("No pain", "Pain"), lwd = 2)
-
-plot(delta_diameter_mm ~ index, data = data_delta_fullsleep_agg_agg, type = "n", frame.plot = F, xaxt = "n", yaxt = "n", ylim = c(-0.25, 0.05), main = "", xlab = "Time, s", ylab = "Diameter change, mm")
-abline(v = 95, lty = 2)
-polygon(x = c(40, 40, 75, 75), y = c(1, -1, -1, 1), col = "gray", border = "gray")
-axis(1, at = c(0, 40, 75, 95, 140), labels = c(-4, 0, 3.5, 5.5, 10))
-axis(2, at = c(-0.25, -0.15, -0.05, 0.05))
-lines(delta_diameter_mm ~ index, data = data_delta_fullsleep_agg_agg, col = "blue", lwd = 2)
-lines(delta_diameter_mm ~ index, data = data_delta_sleepdeprived_agg_agg, col = "red", lty = 2, lwd = 2)
-legend("bottomright", lty = c(1, 2), col = c("blue", "red"), legend = c("Full Sleep", "Sleep Deprived"), lwd = 2)
-dev.off()
