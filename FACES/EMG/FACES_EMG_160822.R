0bef5f331bd3c436641171479caa1c0b6c8c542a
Gustav Nilsonne <gustav.nilsonne@ki.se>
2018-09-25 13:39:42 +0200

    update

--- FACES/EMG/FACES_EMG_160822.R
+++ FACES/EMG/FACES_EMG_160822.R
@@ -8,7 +8,7 @@ cols <- brewer.pal(3,"Dark2")
 
 # Read data
 # Define files
-files <- list.files("C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles/Acqknowledge_logfiles_exported/")
+files <- list.files("C:/Users/gusta/Box Sync/Sleepy Brain/Datafiles/Acqknowledge_logfiles_exported/")
 files <- files[-grep(files, pattern = "excluded")] # Remove files from participants excluded from the experiment altogether
 files <- files[-length(files)] # Remove last, provisional due to zipped archive present in folder
 
@@ -21,7 +21,7 @@ files <- files[files != "9025_2.txt"] # Remove file where reference wave was on 
 
 # Loop over data files, read them, write check plots for reference waves, regress the signals on one another
 for(i in 1:length(files)){
-  data <- read.delim(paste("C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles/Acqknowledge_logfiles_exported/", files[i], sep = ""), skip = 19, header = F)
+  data <- read.delim(paste("C:/Users/gusta/Box Sync/Sleepy Brain/Datafiles/Acqknowledge_logfiles_exported/", files[i], sep = ""), skip = 19, header = F)
   data <- data[, c(1, 4:8)]
   subject_session <- substr(files[i], 1, 6)
   plot(data$V6, type = "l", frame.plot = F, col = "gray", xlab = "Time (min)", ylab = "", xaxt = "n", main = subject_session)
@@ -183,9 +183,9 @@ qqline(blockdata$log_corr_resid)
 # Data are not well approximating a normal distribution, but it looks like a log transformation won't help
 
 # Read demographic and other data, add, prepare for modelling
-demdata <- read.csv2("C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles/demdata_160225_pseudonymized.csv")
+demdata <- read.csv2("C:/Users/gusta/Box Sync/Sleepy Brain/Datafiles/demdata_160225_pseudonymized.csv")
 blockdata <- merge(blockdata, demdata[, c("id", "AgeGroup", "Sl_cond")], by.x = "subject", by.y = "id")
-subjects <- read.csv2("C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafiles/Subjects_151215.csv")
+subjects <- read.csv2("C:/Users/gusta/Box Sync/Sleepy Brain/Datafiles/Subjects_151215.csv")
 blockdata <- merge(blockdata, subjects[, c("SuccessfulIntervention", "newid")], by.x = "subject", by.y = "newid")
 
 blockdata$condition <- "fullsleep"
@@ -194,15 +194,29 @@ blockdata$condition[blockdata$session == 2 & blockdata$Sl_cond == 2] <- "sleepde
 blockdata$condition <- as.factor(blockdata$condition)
 blockdata$condition <- relevel(blockdata$condition, ref = "fullsleep")
 blockdata$AgeGroup <- relevel(blockdata$AgeGroup, ref = "Young")
+blockdata$corr_resid[blockdata$subject_session %in% rejectedcorr] <- NA
+
+# Contrast coding
+contrasts(blockdata$stimulus)[, 1] <- c(-0.5, 1, -0.5) # Will test angry vs neutral and happy
+contrasts(blockdata$stimulus)[, 2] <- c(-0.5, -0.5, 1) # Will test happy vs neutral and angry
+contrasts(blockdata$condition) <- c(-0.5, 0.5)
+contrasts(blockdata$AgeGroup) <- c(-0.5, 0.5)
+
+# Make data frame with only final sample
+blockdata2 <- blockdata[!is.na(blockdata$SuccessfulIntervention), ]
 
 # Build and inspect regression models
-lme1 <- lme(zyg_resid ~ stimulus*condition*AgeGroup, data = blockdata, random = ~ 1|subject/session/block, na.action = na.omit)
+lme1 <- lme(zyg_resid ~ stimulus*condition*AgeGroup, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
 plot(lme1)
 plot(effect("stimulus", lme1))
 plot(effect("stimulus*condition*AgeGroup", lme1))
 summary(lme1)
+sink(file = "FACES/EMG/zyg_regression_output.txt") 
+summary(lme1) 
+intervals(lme1, which = "fixed")
+sink() 
 
-lme1b <- lme(zyg_resid ~ stimulus*condition*AgeGroup, data = blockdata[blockdata$stimulus != "neutral" & !is.na(blockdata$SuccessfulIntervention), ], random = ~ 1|subject/session/block, na.action = na.omit)
+lme1b <- lme(zyg_resid ~ stimulus*condition*AgeGroup, data = blockdata2[blockdata2$stimulus != "neutral", ], random = ~ 1|subject/session/block, na.action = na.omit)
 plot(lme1b)
 plot(effect("stimulus", lme1b))
 plot(effect("stimulus*condition", lme1b))
@@ -213,13 +227,37 @@ lme1c <- lme(zyg_resid ~ stimulus, data = blockdata, random = ~ 1|subject/sessio
 plot(lme1c)
 plot(effect("stimulus", lme1c))
 summary(lme1c)
-intervals(lme1c)
+intervals(lme1c, which = "fixed")
 
-lme2 <- lme(corr_resid ~ stimulus*condition*AgeGroup, data = blockdata, random = ~ 1|subject/session/block, na.action = na.omit)
+lme2 <- lme(corr_resid ~ stimulus*condition*AgeGroup, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
 plot(lme2)
 plot(effect("stimulus", lme2))
 plot(effect("stimulus*condition*AgeGroup", lme2))
 summary(lme2)
+sink(file = "FACES/EMG/corr_regression_output.txt") 
+summary(lme2) 
+intervals(lme2, which = "fixed")
+sink() 
+
+lme2bb <- lme(corr_resid ~ stimulus*condition, data = blockdata2[blockdata2$AgeGroup == "Young", ], random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2bb)
+plot(effect("stimulus", lme2bb))
+plot(effect("stimulus*condition", lme2bb))
+summary(lme2bb)
+sink(file = "FACES/EMG/corr_regression_output_younger.txt") 
+summary(lme2bb) 
+intervals(lme2bb, which = "fixed")
+sink() 
+
+lme2bc <- lme(corr_resid ~ stimulus*condition, data = blockdata2[blockdata2$AgeGroup == "Old", ], random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2bc)
+plot(effect("stimulus", lme2bc))
+plot(effect("stimulus*condition", lme2bc))
+summary(lme2bc)
+sink(file = "FACES/EMG/corr_regression_output_older.txt") 
+summary(lme2bc) 
+intervals(lme2bc, which = "fixed")
+sink() 
 
 lme2b <- lme(corr_resid ~ stimulus*condition*AgeGroup, data = blockdata[blockdata$stimulus != "neutral" & !is.na(blockdata$SuccessfulIntervention & !(blockdata$subject_session %in% rejectedcorr)), ], random = ~ 1|subject/session/block, na.action = na.omit)
 plot(lme2b)
@@ -277,6 +315,72 @@ lines(x = c(0, 1, 2), y = effect("stimulus", lme2c)$fit[c(3, 1, 2)]*1000, pch = 
 lines(x = c(0, 0), y = c(effect("stimulus", lme2c)$lower[3]*1000, effect("stimulus", lme2c)$upper[3]*1000), lwd = 1.5)
 lines(x = c(1, 1), y = c(effect("stimulus", lme2c)$lower[1]*1000, effect("stimulus", lme2c)$upper[1]*1000), lwd = 1.5)
 lines(x = c(2, 2), y = c(effect("stimulus", lme2c)$lower[2]*1000, effect("stimulus", lme2c)$upper[2]*1000), lwd = 1.5)
+dev.off()
+
+eff <- effect("stimulus*condition*AgeGroup", lme1)
+eff$fit <- eff$fit*1000
+eff$upper <- eff$upper*1000
+eff$lower <- eff$lower*1000
+
+eff2 <- effect("stimulus*condition*AgeGroup", lme2)
+eff2$fit <- eff2$fit*1000
+eff2$upper <- eff2$upper*1000
+eff2$lower <- eff2$lower*1000
+
+pdf("FACES/EMG/EMG_main2.pdf", height = 5, width = 5) 
+
+plot(1, frame.plot = F, xlim = c(-0.1, 2.1), ylim = c(min(eff$lower), 0.1), xlab = "stimulus", ylab = "microV, residual", xaxt = "n", yaxt = "n", type = "n", main = "Zygomatic, young")
+axis(1, at = c(0, 1, 2), labels = c("Happy", "Neutral", "Angry"))
+axis(2, at = c(-0.1, -0.05, 0, 0.05, 0.1))
+lines(x = c(-0.05, 0.95, 1.95), y = eff$fit[c(3, 1, 2)], pch = 16, type = "b", lwd = 1.5, col = cols[2])
+lines(x = c(-0.05, -0.05), y = c(eff$lower[3], eff$upper[3]), lwd = 1.5, col = cols[2])
+lines(x = c(0.95, 0.95), y = c(eff$lower[1], eff$upper[1]), lwd = 1.5, col = cols[2])
+lines(x = c(1.95, 1.95), y = c(eff$lower[2], eff$upper[2]), lwd = 1.5, col = cols[2])
+lines(x = c(0.05, 1.05, 2.05), y = eff$fit[c(6, 4, 5)], pch = 15, type = "b", lwd = 1.5, col = cols[3])
+lines(x = c(0.05, 0.05), y = c(eff$lower[6], eff$upper[6]), lwd = 1.5, col = cols[3])
+lines(x = c(1.05, 1.05), y = c(eff$lower[4], eff$upper[4]), lwd = 1.5, col = cols[3])
+lines(x = c(2.05, 2.05), y = c(eff$lower[5], eff$upper[5]), lwd = 1.5, col = cols[3])
+legend("top", lwd = 1.5, pch = c(16, 15), legend = c("full sleep", "sleep deprivation"), col = cols[c(2, 3)], bty = "n")
+
+plot(1, frame.plot = F, xlim = c(-0.1, 2.1), ylim = c(min(eff$lower), 0.1), xlab = "stimulus", ylab = "microV, residual", xaxt = "n", yaxt = "n", type = "n", main = "Zygomatic, older")
+axis(1, at = c(0, 1, 2), labels = c("Happy", "Neutral", "Angry"))
+axis(2, at = c(-0.1, -0.05, 0, 0.05, 0.1))
+lines(x = c(-0.05, 0.95, 1.95), y = eff$fit[c(9, 7, 8)], pch = 16, type = "b", lwd = 1.5, col = cols[2])
+lines(x = c(-0.05, -0.05), y = c(eff$lower[9], eff$upper[9]), lwd = 1.5, col = cols[2])
+lines(x = c(0.95, 0.95), y = c(eff$lower[7], eff$upper[7]), lwd = 1.5, col = cols[2])
+lines(x = c(1.95, 1.95), y = c(eff$lower[8], eff$upper[8]), lwd = 1.5, col = cols[2])
+lines(x = c(0.05, 1.05, 2.05), y = eff$fit[c(12, 10, 11)], pch = 15, type = "b", lwd = 1.5, col = cols[3])
+lines(x = c(0.05, 0.05), y = c(eff$lower[12], eff$upper[12]), lwd = 1.5, col = cols[3])
+lines(x = c(1.05, 1.05), y = c(eff$lower[10], eff$upper[10]), lwd = 1.5, col = cols[3])
+lines(x = c(2.05, 2.05), y = c(eff$lower[11], eff$upper[11]), lwd = 1.5, col = cols[3])
+legend("top", lwd = 1.5, pch = c(16, 15), legend = c("full sleep", "sleep deprivation"), col = cols[c(2, 3)], bty = "n")
+
+plot(1, frame.plot = F, xlim = c(-0.1, 2.1), ylim = c(-0.5, 0.5), xlab = "stimulus", ylab = "microV, residual", xaxt = "n", yaxt = "n", type = "n", main = "Corrugator, young")
+axis(1, at = c(0, 1, 2), labels = c("Happy", "Neutral", "Angry"))
+axis(2, at = c(-0.5, 0, 0.5))
+lines(x = c(-0.05, 0.95, 1.95), y = eff2$fit[c(3, 1, 2)], pch = 16, type = "b", lwd = 1.5, col = cols[2])
+lines(x = c(-0.05, -0.05), y = c(eff2$lower[3], eff2$upper[3]), lwd = 1.5, col = cols[2])
+lines(x = c(0.95, 0.95), y = c(eff2$lower[1], eff2$upper[1]), lwd = 1.5, col = cols[2])
+lines(x = c(1.95, 1.95), y = c(eff2$lower[2], eff2$upper[2]), lwd = 1.5, col = cols[2])
+lines(x = c(0.05, 1.05, 2.05), y = eff2$fit[c(6, 4, 5)], pch = 15, type = "b", lwd = 1.5, col = cols[3])
+lines(x = c(0.05, 0.05), y = c(eff2$lower[6], eff2$upper[6]), lwd = 1.5, col = cols[3])
+lines(x = c(1.05, 1.05), y = c(eff2$lower[4], eff2$upper[4]), lwd = 1.5, col = cols[3])
+lines(x = c(2.05, 2.05), y = c(eff2$lower[5], eff2$upper[5]), lwd = 1.5, col = cols[3])
+legend("top", lwd = 1.5, pch = c(16, 15), legend = c("full sleep", "sleep deprivation"), col = cols[c(2, 3)], bty = "n")
+
+plot(1, frame.plot = F, xlim = c(-0.1, 2.1), ylim = c(-0.5, 0.5), xlab = "stimulus", ylab = "microV, residual", xaxt = "n", yaxt = "n", type = "n", main = "Corrugator, older")
+axis(1, at = c(0, 1, 2), labels = c("Happy", "Neutral", "Angry"))
+axis(2, at = c(-0.5, 0, 0.5))
+lines(x = c(-0.05, 0.95, 1.95), y = eff2$fit[c(9, 7, 8)], pch = 15, type = "b", lwd = 1.5, col = cols[3])
+lines(x = c(-0.05, -0.05), y = c(eff2$lower[9], eff2$upper[9]), lwd = 1.5, col = cols[3])
+lines(x = c(0.95, 0.95), y = c(eff2$lower[7], eff2$upper[7]), lwd = 1.5, col = cols[3])
+lines(x = c(1.95, 1.95), y = c(eff2$lower[8], eff2$upper[8]), lwd = 1.5, col = cols[3])
+lines(x = c(0.05, 1.05, 2.05), y = eff2$fit[c(12, 10, 11)], pch = 16, type = "b", lwd = 1.5, col = cols[2])
+lines(x = c(0.05, 0.05), y = c(eff2$lower[12], eff2$upper[12]), lwd = 1.5, col = cols[2])
+lines(x = c(1.05, 1.05), y = c(eff2$lower[10], eff2$upper[10]), lwd = 1.5, col = cols[2])
+lines(x = c(2.05, 2.05), y = c(eff2$lower[11], eff2$upper[11]), lwd = 1.5, col = cols[2])
+legend("top", lwd = 1.5, pch = c(16, 15), legend = c("full sleep", "sleep deprivation"), col = cols[c(2, 3)], bty = "n")
+
 dev.off()
 
 # Analyse again with difference scores to reduce model complexity
@@ -361,3 +465,136 @@ subjectlist <- read.csv2("C:/Users/Gustav Nilsonne/Box Sync/Sleepy Brain/Datafil
 temp2 <- merge(temp, subjectlist[, c("Subject", "newid")])
 temp2 <- temp2[, -1]
 write.csv(temp2, "EMG_diff_2.csv", row.names = FALSE)
+
+# Analyse habituation
+lme1e <- lme(zyg_resid ~ stimulus*condition*AgeGroup + block, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme1e)
+plot(effect("block", lme1e))
+summary(lme1e)
+sink(file = "FACES/EMG/zyg_regression_output_habituation.txt") 
+summary(lme1e) 
+intervals(lme1e, which = "fixed")
+sink() 
+
+lme2e <- lme(corr_resid ~ stimulus*condition*AgeGroup + block, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2e)
+plot(effect("block", lme2e))
+summary(lme2e)
+sink(file = "FACES/EMG/corr_regression_output_habituation.txt") 
+summary(lme2e) 
+intervals(lme2e, which = "fixed")
+sink() 
+
+# Analyse covariates
+ratings <- read.csv("FACES/EMG/ratings.csv")
+ratings_happy <- ratings[ratings$Question_type == 2, ]
+ratings_angry <- ratings[ratings$Question_type == 3, ]
+blockdata2_happyratings <- merge(blockdata2, ratings_happy, by.x = c("subject", "session", "block"), by.y = c("newid", "session", "block"))
+blockdata2_angryratings <- merge(blockdata2, ratings_angry, by.x = c("subject", "session", "block"), by.y = c("newid", "session", "block"))
+
+lme1f <- lme(zyg_resid ~ stimulus*Rating, data = blockdata2_happyratings, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme1f)
+plot(effect("Rating", lme1f))
+summary(lme1f)
+sink(file = "FACES/EMG/zyg_regression_output_happy_rating.txt") 
+summary(lme1f) 
+intervals(lme1f, which = "fixed")
+sink() 
+
+lme2f <- lme(corr_resid ~ stimulus*Rating, data = blockdata2_happyratings, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2f)
+plot(effect("Rating", lme2f))
+summary(lme2f)
+sink(file = "FACES/EMG/corr_regression_output_happy_rating.txt") 
+summary(lme2f) 
+intervals(lme2f, which = "fixed")
+sink() 
+
+lme1g <- lme(zyg_resid ~ stimulus*Rating, data = blockdata2_angryratings, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme1g)
+plot(effect("Rating", lme1g))
+summary(lme1g)
+sink(file = "FACES/EMG/zyg_regression_output_angry_rating.txt") 
+summary(lme1g) 
+intervals(lme1g, which = "fixed")
+sink() 
+
+lme2g <- lme(corr_resid ~ stimulus*Rating, data = blockdata2_angryratings, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2g)
+plot(effect("Rating", lme2g))
+summary(lme2g)
+sink(file = "FACES/EMG/corr_regression_output_angry_rating.txt") 
+summary(lme2g) 
+intervals(lme2g, which = "fixed")
+sink() 
+
+blockdata2 <- merge(blockdata2, demdata[, c("id", "IRI_EC", "PANAS_Positive_byScanner.x", "PANAS_Positive_byScanner.y", "PANAS_Negative_byScanner.x", "PANAS_Negative_byScanner.y",
+                                            "PSS14", "PPIR_C")], by.x = "subject", by.y = "id")
+
+lme1h <- lme(zyg_resid ~ stimulus*IRI_EC, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme1h)
+plot(effect("IRI_EC", lme1h))
+summary(lme1h)
+sink(file = "FACES/EMG/zyg_regression_output_IRI_EC.txt") 
+summary(lme1h) 
+intervals(lme1h, which = "fixed")
+sink() 
+
+lme2h <- lme(corr_resid ~ stimulus*IRI_EC, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2h)
+plot(effect("IRI_EC", lme2h))
+summary(lme2h)
+sink(file = "FACES/EMG/corr_regression_output_IRI_EC.txt") 
+summary(lme2h) 
+intervals(lme2h, which = "fixed")
+sink() 
+
+
+
+
+
+lme1k <- lme(zyg_resid ~ stimulus*PSS14, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme1k)
+plot(effect("PSS14", lme1k))
+summary(lme1k)
+sink(file = "FACES/EMG/zyg_regression_output_PSS14.txt") 
+summary(lme1k) 
+intervals(lme1k, which = "fixed")
+sink() 
+
+lme2k <- lme(corr_resid ~ stimulus*PSS14, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2k)
+plot(effect("PSS14", lme2k))
+summary(lme2k)
+sink(file = "FACES/EMG/corr_regression_output_PSS14.txt") 
+summary(lme2k) 
+intervals(lme2k, which = "fixed")
+sink()
+
+lme1l <- lme(zyg_resid ~ stimulus*PPIR_C, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme1l)
+plot(effect("PPIR_C", lme1l))
+summary(lme1l)
+sink(file = "FACES/EMG/zyg_regression_output_PPIR_C.txt") 
+summary(lme1l) 
+intervals(lme1l, which = "fixed")
+sink() 
+
+lme2l <- lme(corr_resid ~ stimulus*PPIR_C, data = blockdata2, random = ~ 1|subject/session/block, na.action = na.omit)
+plot(lme2l)
+plot(effect("PPIR_C", lme2l))
+summary(lme2l)
+sink(file = "FACES/EMG/corr_regression_output_PPIR_C.txt") 
+summary(lme2l) 
+intervals(lme2l, which = "fixed")
+sink()
+
+
+#############################
+EMG responses will be predicted by 
+- participants’ rated happiness and angriness during the experiment
+- participants’ ratings on 
+    - the Interpersonal Reactivity Index Empathic Concern subscale (IRI-EC)
+    - the Positive and Negative Affect Scale (PANAS)
+    - the Perceived Stress Scale (PSS), and 
+    - inversely predicted by the Psychopathic Personality Index-Revised Coldheartedness subscale (PPI-R-C).
