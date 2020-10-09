05822836a921e425edbcc4500992753a63fa3aca
Sandra Tamm <sandra.tamm@stressforskning.su.se>
2020-10-09 11:25:23 +0100

    parameter estimates

--- Sleepmeasures_emotion/Scripts/Multilevel_SEM.R
+++ Sleepmeasures_emotion/Scripts/Multilevel_SEM.R
@@ -1,4 +1,4 @@
-
+library(lavaan)
 Data <- read.csv("~/Desktop/SleepyBrain-Analyses/Sleepmeasures_emotion/Data/SEM_Singer_standardized.csv")
 Data$AgeGroup <- as.numeric(Data$AgeGroup)  
 Data$DeprivationCondition <- as.numeric(Data$DeprivationCondition)
@@ -15,8 +15,18 @@ model <- "
 # Missing values!
 fit <- sem(model, Data, cluster = "Subject")
 summary(fit, fit.measures=TRUE)
+parameterEstimates(fit, standardized = TRUE)
+
 
 setwd("/Users/santam/Desktop/SleepyBrain-Analyses/Sleepmeasures_emotion/Output")
 sink("Multilevel_SEM.txt")
 summary(fit, fit.measures=TRUE)
+sink()
+
+sink("parameterEstimates_SEM_uns.txt")
+parameterEstimates(fit, standardized = FALSE)
+sink()
+
+sink("parameterEstimates_SEM_std.txt")
+parameterEstimates(fit, standardized = TRUE)
 sink()
